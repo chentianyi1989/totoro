942c3b49a5965b9a386c304e3b9721a6858301f6
chenty <chenty@chenty-PC>
2017-08-16 02:07:41 +0800

    update：composer update , add

---
+++ app/Models/Test1.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test1 extends Model{
+    
+    protected $table = 'test1';
+    
+    protected $guarded = [];
+    
+}
